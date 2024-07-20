a287e54234c22759e54fa47e44c66f193cee030e
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-07-20 20:01:57 -0300

    update

--- src/components/GlobalAudio.vue
+++ src/components/GlobalAudio.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <audio ref="audioPlayer" autoplay loop>
-      <source src="fita.mp3" type="audio/mp3">
+      <source :src="audioSource" type="audio/mp3">
       Your browser does not support the audio element.
     </audio>
   </div>
@@ -10,6 +10,11 @@
 <script>
 export default {
   name: 'GlobalAudio',
+  data() {
+    return {
+      audioSource: process.env.NODE_ENV === 'production' ? '/Lar-dos-Fita/fita.mp3' : '/fita.mp3'
+    };
+  },
   methods: {
     playAudio() {
       this.$refs.audioPlayer.play().then(() => {
